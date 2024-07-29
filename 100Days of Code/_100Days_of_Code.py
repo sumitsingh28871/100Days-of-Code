62cd3155c2f277bdd4d8e212eaecdf38fe44c664
SumitSingh <sumitsingh28871@gmail.com>
2024-07-29 22:54:51 +0100

    changes saved successfully

--- 100Days of Code/_100Days_of_Code.py
+++ 100Days of Code/_100Days_of_Code.py
@@ -1,1 +1,11 @@
-#
+#Greetins of the program
+print ("Welcome to the Band Name Generator")
+
+#Ask the user about their city they grew up in
+city = input("In which city you grew up in?\n")
+
+#Ask the user about their pet name
+pet = input("Whats the name of your pet?\n")
+
+#generate a band name
+print ("Woohoo your Band Name could be" + " " + city + pet )
